609b9e2f4e63a75dbfe29f5fa30caa79f3d82568
Dipak Kumar Mondal <dipak.cse2004@gmail.com>
2026-08-22 17:53:23 -0300

    Update Appliances.java

--- src/main/java/com/smart/appliances/control/Appliances.java
+++ src/main/java/com/smart/appliances/control/Appliances.java
@@ -1,5 +1,7 @@
 package com.smart.appliances.control;
 
+// common interface for all appliances
 public interface Appliances {
+    // common turnOff() method for all appliances and implementation depends on the appliance
     void turnOff();
 }
